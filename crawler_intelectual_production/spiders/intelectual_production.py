7344c8670d44077873be1cd2f415377a1ef60975
André Alba <andre90012002@gmail.com>
2021-06-06 16:55:21 -0300

    add 35k intelectual productino crawled, add documentation in the code, removed not used package

--- crawler_intelectual_production/spiders/intelectual_production.py
+++ crawler_intelectual_production/spiders/intelectual_production.py
@@ -1,23 +1,24 @@
 import scrapy
-from urllib.parse import urlparse
-import json
 from crawler_intelectual_production.items import CrawlerIntelectualProductionItem
 import pandas as pd 
 
-
+# 
 class intelectual_production(scrapy.Spider):
     name = "intelectual_production"
      
     def start_requests(self):
         urls = list()
-
+        
+        # Open json file with downloaded  production, get the index and trasnform float numbers in int64
         df = pd.read_json('test.json')['site_id'].astype(int)
         
+        # create range of index that i will crawling, and merge with downloaded index production in the last turn that i runed the crawler 
         df = df.append(pd.Series(range(90000,900000)), ignore_index=True)
-        # df = pd.Series(range(95000,900000))
+        
+        # Drop all index duplicates for criate dataframe with only not crawled index
         df = df.drop_duplicates(keep=False)
 
-        # get 90000 to 900000
+        # create all links that i will crawling  
         for i in df:
             urls.append(
                 "https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/producaoIntelectual/viewProducaoIntelectual.jsf?popup=true&id_producao="+str(i)
@@ -31,9 +32,12 @@ class intelectual_production(scrapy.Spider):
 
     def parse(self, response):
 
+        # instanciate item calass
         intel_prod_data = CrawlerIntelectualProductionItem()
 
-
+        # teste if de addres crawled had information 
+        # if YES, get the information, set id_status with True and call yield 
+        # else set id_status with False and call yield
         if len(response.xpath('//*[@id="form:nome"]/text()'))>0:
             intel_prod_data['title_production'] = response.xpath('//*[@id="form:nome"]/text()').extract_first()
             intel_prod_data['site_id'] = response.url.split('=')[2]
